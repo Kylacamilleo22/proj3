c529c06a3695f68ba822b28ae5a7b7edb5a5db0b
Kyla Camille Ocampo <kylacamilleocamp@onyx.boisestate.edu>
2024-09-26 17:04:28 -0600

    task 4 added

--- app/main.c
+++ app/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
-#include<string.h>
-#include "../src/lab.c"
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../src/lab.h"
 
 int terminal_count () {
   static int shell_count = 0;
@@ -18,47 +20,23 @@ int main(int argc, char **argv)
     
     // char *name = "MY_PROMPT";
     // char *env = getenv(name);
-    char *env = "MY_PROMPT";
-    const char *prompt = getenv(env);
-  char *line;
 
- while ((c = getopt (argc, argv, "vbc:")) != -1) { 
-  switch (c)
-  {
-  case 'v':
-    /* code */
-    printf("Shell version: %d.%d", lab_VERSION_MAJOR, lab_VERSION_MINOR);
-    printf("prompt: %s", prompt);
-    exit(0);
-  case 'b':
-    printf("get b here");
-    
-    break;
-  case 'c':
-    printf("get c here");
-    break;
-  // case '?':
-  //   if (isprint (optopt)) {
-  //     fprintf(stderr, "Unknown option", optopt ); 
-  //   } else {
-  //     fprintf(stderr, "Uknown option", optopt);
-  //   }
-  //   return 1;
-  default:
-    // will run here if no other arguments are included
-    // such as ./myprogram or MY_PROMPT="foo>" ./myprogram
-    //getprompt here, calling get_prompt
-    // printf("%s", env);
-        printf("hello");
+    char *line;
+
+    // if -v is declared, print the version
+    parse_args(argc, argv);
+
+    // get prompt from environment
+    const char *env = "MY_PROMPT";
+    const char *prompt = getenv(env);
 
     using_history();
-    while ((line=readline("$"))){
-        printf("%s\n",line);
-        add_history(line);
-        free(line);
-    }
 
-    
+    while ((line=readline("$"))){
+      printf("%s\n",line);
+      add_history(line);
+      free(line);
+    }
 
     //start the shell
     sh.shell_is_interactive = 1;
@@ -68,8 +46,7 @@ int main(int argc, char **argv)
     // sh.shell_tmodes.c_cc = 
 
     
-    break;
-  }
- }
+
+
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,36 @@
-/**Update this file with the starter code**/
+#include "../src/lab.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
+
+
+  void parse_args(int argc, char **argv){
+    // int numArgs = 0;
+    // for (int i = 0; i < argc; i++) {
+    //     numArgs++;
+
+    //     printf("arguments: %c", *argv[i]);
+
+    // }   
+    // printf("%d %d", argc,**argv);
+    int c;  
+    while ((c = getopt (argc, argv, "v")) != -1) { 
+    switch (c)
+    {
+    case 'v':
+        /* code */
+        printf("Shell version: %d.%d", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+        exit(0);
+    default:
+        printf("Unknown command, exiting now");
+        exit(0);
+    }
+   
+}
+  }
