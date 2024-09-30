f97cb77ae887546f8d6c83eeec036b4347a68fba
Kyla Camille Ocampo <kylacamilleocamp@onyx.boisestate.edu>
2024-09-30 17:11:50 -0600

    get_prompt implemented

--- app/main.c
+++ app/main.c
@@ -7,16 +7,17 @@
 #include <readline/history.h>
 #include "../src/lab.h"
 
-int terminal_count () {
-  static int shell_count = 0;
-  return shell_count++;
-}
+// int terminal_count () {
+//   static int shell_count = 0;
+//   return shell_count++;
+// }
 
 int main(int argc, char **argv)
 { 
   // printf("hello world\n");
-  int c;
-    struct shell sh;
+  
+    struct shell *sh;
+    const char *prompt_default = "shell>";
     
     // char *name = "MY_PROMPT";
     // char *env = getenv(name);
@@ -24,28 +25,54 @@ int main(int argc, char **argv)
     char *line;
 
     // if -v is declared, print the version
-    parse_args(argc, argv);
+    //parse_args(argc, argv);
+    int c;  
+    while ((c = getopt (argc, argv, "v")) != -1) { 
+      switch (c)
+      {
+      case 'v':
+          /* code */
+          printf("Shell version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+          exit(0);
+      default:
+          printf("Unknown command, exiting now");
+          exit(0);
+      }
+    }
 
     // get prompt from environment
     const char *env = "MY_PROMPT";
-    const char *prompt = getenv(env);
+    const char *prompt = get_prompt(env);
 
+    //start the shell
+    // need to implement sh_init
+    // sh->shell_is_interactive = 1;
+    // sh->prompt = prompt;
+    // sh->shell_pgid = getpid();
+    // sh->shell_terminal = terminal_count();
+    // sh->shell_tmodes.c_iflag = 0;
+
+    // If no prompt provided, then use shell>
+    // if (!prompt) {
+    //   prompt = prompt_default;
+    // } 
+    // printf("%s\n", prompt);
+    
     using_history();
-
-    while ((line=readline("$"))){
+    
+    while ((line=readline(prompt))){
       printf("%s\n",line);
+
+      // convert line to execvp format
+
+
+      // check if command is built in command
+      // do_builtin(sh, )
+
       add_history(line);
       free(line);
     }
 
-    //start the shell
-    sh.shell_is_interactive = 1;
-    sh.prompt = prompt;
-    sh.shell_pgid = getpid();
-    sh.shell_terminal = terminal_count();
-    // sh.shell_tmodes.c_cc = 
-
-    
 
 
   return 0;
--- src/lab.c
+++ src/lab.c
@@ -8,7 +8,20 @@
 #include <readline/history.h>
 #include <signal.h>
 
+  const char *prompt_default = "shell>";
+  
+  int terminal_count () {
+  static int shell_count = 0;
+  return shell_count++;
+  }
 
+  void sh_init(struct shell *sh) {
+    sh->shell_is_interactive = 1;
+    sh->prompt = NULL;
+    sh->shell_pgid = getpid();
+    sh->shell_terminal = terminal_count();
+    sh->shell_tmodes.c_iflag = 0;
+  }
 
   void parse_args(int argc, char **argv){
     // int numArgs = 0;
@@ -33,4 +46,14 @@
     }
    
 }
+  }
+
+  char *get_prompt(const char *env) {
+    const char *prompt = getenv(env);
+
+    if (!prompt) {
+      prompt = prompt_default;
+    }
+
+    return prompt;
   }
